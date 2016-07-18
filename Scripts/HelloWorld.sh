52a86b1ed7d4c3e7d0719938bc17b3a3f71dd713
Jonas Lindgren <dproine@gmail.com>
2016-07-18 23:04:34 +0200

    Create HelloWorld.sh
    First file, just to have something to download and execute..

---
+++ Scripts/HelloWorld.sh
@@ -0,0 +1,5 @@
+# This is the first file that should be downloaded and run on the target server. 
+# Initially, I have a ubuntu server installed on virtualBox, so the first version 
+# of this file is just to find out how to download this file and execute it.  
+
+echo "Hello, World!" 
