e97647c6c56b25f959592dcc6ad1073ed67efccd
Jonas Lindgren <dproine@gmail.com>
2016-07-18 23:20:08 +0200

    Adding install of git

--- Scripts/HelloWorld.sh
+++ Scripts/HelloWorld.sh
@@ -2,4 +2,15 @@
 # Initially, I have a ubuntu server installed on virtualBox, so the first version 
 # of this file is just to find out how to download this file and execute it.  
 
-echo "Hello, World!" 
+# DON'T DO THIS, IT'S REALLY STUPID TO JUST DOWNLOAD SOME CODE FROM THE INTERNET AND EXECUTE IT!
+# to execute this file in target server, execute:
+# wget "https://raw.githubusercontent.com/R01Ne/AutoCI/master/Scripts/HelloWorld.sh"
+# chmod +x ./HelloWorld.sh
+# sudo ./HelloWorld.sh
+
+echo "Hello, soon-to-be-CI-user!"
+echo "We'll need git to get some more scripts.."
+
+# Get the latest info about packages
+apt-get update
+apt get install git
